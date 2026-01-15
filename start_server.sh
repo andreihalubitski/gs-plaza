7c832aa32a3c0913ddb0f841b1a8b6ef41a5f55f
andreihalubitski <andreihalubitski@gmail.com>
2026-01-15 11:43:40 +0300

    Delete start_server.sh

--- start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Запуск локального сервера для сайта..."
-cd /workspace/html
-echo "Сайт будет доступен по адресу: http://localhost:8000/real-estate.html"
-python3 -m http.server 8000
